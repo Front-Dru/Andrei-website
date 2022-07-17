34ad0b2d995d379907c9c45a72165b7fbc2502fd
TakkMai <ivanoval.ru@yandex.ru>
2022-07-17 21:01:40 +0200

    first commit

---
+++ telegram.php
@@ -0,0 +1,30 @@
+<?php
+
+/* https://api.telegram.org/bot5379176577:AAEh0u8kO5vkTZu_uJw58z9ljejuYilURnc/getUpdates,
+где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+$msg = $_POST['msg'];
+$token = "5379176577:AAEh0u8kO5vkTZu_uJw58z9ljejuYilURnc";
+$chat_id = "-511596153";
+$arr = array(
+  'Имя пользователя: ' => $name,
+  'Телефон: ' => $phone,
+  'Email' => $email,
+  'Сообщение:' => $msg
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+if ($sendToTelegram ) {
+  header('Location: thanks.html');
+} else {
+  echo "Error";
+}
+?>
